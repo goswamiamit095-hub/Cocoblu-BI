5ca03acb8df52d98b3b96053dca4387d34ea29b4
goswamiamit095-hub <goswamiamit095@gmail.com>
2026-07-02 19:29:21 +0530

    Update routes.js

---
+++ config/routes.js
@@ -0,0 +1,30 @@
+/*==================================================
+    COCOBLU BI
+    ROUTE CONFIG
+==================================================*/
+
+const ROUTES = {
+
+    home: {
+        title: "Business Overview",
+        file: "pages/dashboard/home/home.html"
+    },
+
+    "monthly-summary": {
+        title: "Monthly Summary",
+        file: "pages/sales/monthly-summary/monthly-summary.html"
+    },
+
+    "daily-summary": {
+        title: "Daily Summary",
+        file: "pages/sales/daily-summary/daily-summary.html"
+    },
+
+    "asin-sales": {
+        title: "ASIN Sales",
+        file: "pages/sales/asin-sales/asin-sales.html"
+    }
+
+};
+
+window.ROUTES = ROUTES;
